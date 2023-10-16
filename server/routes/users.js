fa9785bb4e4bc13460ace63623ff747a5ad45fd5
Pavan-Asopa <pavanasopa14@gmail.com>
2023-10-17 07:03:25 +1000

    code added for create s3 bucket

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const AWS = require('aws-sdk');
+
+// Create unique bucket name
+const bucketName = "";
+const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+
+(async () => {
+  try {
+    await s3.createBucket({ Bucket: bucketName }).promise();
+    console.log(`Created bucket: ${bucketName}`);
+  } catch (err) {
+    // We will ignore 409 errors which indicate that the bucket already exists
+    if (err.statusCode !== 409) {
+      console.log(`Error creating bucket: ${err}`);
+    }
+  }
+})();
+
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
