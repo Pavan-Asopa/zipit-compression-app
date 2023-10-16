df68454bdc29881217492bad024a9edd7571170f
Pavan-Asopa <pavanasopa14@gmail.com>
2023-10-17 07:18:41 +1000

    added post route to get pre-signed urls

--- server/routes/users.js
+++ server/routes/users.js
@@ -18,11 +18,42 @@ const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
   }
 })();
 
-
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+// Endpoint to get pre-signed URLs for file uploads
+router.post('/getPresignedUrls', (req, res) => {
+  const { numFiles } = req.body; // Number of files to upload
+
+  if (!numFiles) {
+    return res.status(400).json({ error: 'Please specify the number of files to upload' });
+  }
+
+  const preSignedUrls = [];
+
+  // Generate pre-signed URLs for each file
+  for (let i = 0; i < numFiles; i++) {
+    const params = {
+      Bucket: 'YOUR_BUCKET_NAME',
+      Key: `uploads/${Date.now()}-file${i}`, // set the key
+      Expires: 600, // URL expiration time in seconds
+    };
+
+    s3.getSignedUrl('putObject', params, (error, url) => {
+      if (error) {
+        console.error('Error generating pre-signed URL:', error);
+        return res.status(500).json({ error: 'Failed to generate pre-signed URL' });
+      }
+      preSignedUrls.push(url);
+
+      // If all URLs have been generated, return them to the client
+      if (preSignedUrls.length === numFiles) {
+        res.json({ urls: preSignedUrls });
+      }
+    });
+  }
 });
 
 module.exports = router;
