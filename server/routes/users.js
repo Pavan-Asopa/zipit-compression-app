c9a7c9922b16d718e73f4e28731b12f5718f2825
Joanna Salerno <68243975+joannasalerno@users.noreply.github.com>
2023-10-19 09:00:16 +1000

    Adding a few comments

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,46 +1,50 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-require('dotenv').config();
-const AWS = require('aws-sdk');
 
+const AWS = require("aws-sdk");
+require("dotenv").config();
 
 // configure aws sdk
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   sessionToken: process.env.AWS_SESSION_TOKEN,
-  region: 'ap-southeast-2',
+  region: "ap-southeast-2",
 });
 
-// Create unique bucket name
+// create a unique bucket name
 const bucketName = "zipit-storage";
 const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
 (async () => {
   try {
+    // try creating a new bucket
     await s3.createBucket({ Bucket: bucketName }).promise();
     console.log(`Created bucket: ${bucketName}`);
   } catch (err) {
-    if(err.statusCode === 409){
+    // check whether bukcet already exists
+    if (err.statusCode === 409) {
       console.log(`Bucket ${bucketName} already exists`);
-    }else{
-    // We will ignore 409 errors which indicate that the bucket already exists
+    } else {
+      // print any other errors in creating the bucket
       console.log(`Error creating bucket: ${err}`);
     }
   }
 })();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
 // Endpoint to get pre-signed URLs for file uploads using a GET request
-router.get('/getPresignedUrls', (req, res) => {
+router.get("/getPresignedUrls", (req, res) => {
   const { numFiles } = req.query; // Number of files to upload
 
   if (!numFiles) {
-    return res.status(400).json({ error: 'Please specify the number of files to upload' });
+    return res
+      .status(400)
+      .json({ error: "Please specify the number of files to upload" });
   }
 
   const preSignedUrls = [];
@@ -48,15 +52,17 @@ router.get('/getPresignedUrls', (req, res) => {
   // Generate pre-signed URLs for each file
   for (let i = 0; i < numFiles; i++) {
     const params = {
-      Bucket: 'zipit-storage',
+      Bucket: "zipit-storage",
       Key: `uploads/${Date.now()}-file${i}`, // Set the key
       Expires: 600, // URL expiration time in seconds
     };
 
-    s3.getSignedUrl('putObject', params, (error, url) => {
+    s3.getSignedUrl("putObject", params, (error, url) => {
       if (error) {
-        console.error('Error generating pre-signed URL:', error);
-        return res.status(500).json({ error: 'Failed to generate pre-signed URL' });
+        console.error("Error generating pre-signed URL:", error);
+        return res
+          .status(500)
+          .json({ error: "Failed to generate pre-signed URL" });
       }
       preSignedUrls.push(url);
 
@@ -67,6 +73,5 @@ router.get('/getPresignedUrls', (req, res) => {
     });
   }
 });
-
 
 module.exports = router;
