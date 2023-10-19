04385bc5548dcdbe18c712544deee46466dad1ea
Pavan-Asopa <pavanasopa14@gmail.com>
2023-10-19 21:32:16 +1000

    multer stuff

--- client/src/components/Uploader.js
+++ client/src/components/Uploader.js
@@ -106,13 +106,146 @@
 //   );
 // }
 
+// import React, { useState } from 'react';
+// import axios from 'axios';
+
+// const Uploader = () => {
+//   const [name, setName] = useState('');
+//   const [numFiles, setNumFiles] = useState(0);
+//   const [fileUrls, setFileUrls] = useState([]); // To store pre-signed URLs
+//   const [selectedFiles, setSelectedFiles] = useState([]);
+
+//   const handleInputChange = (event) => {
+//     setName(event.target.value);
+//     console.log(`Name is: ${event.target.value}`);
+//   };
+
+//   const handleFileChange = (event) => {
+//     setNumFiles(event.target.value);
+//     console.log(`Will compress ${event.target.value} files`);
+//   };
+
+//   const handleFileSelect = (event) => {
+//     const files = event.target.files;
+//     setSelectedFiles(files);
+//   };
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+  
+//     try {
+//       // Make a request to your server to get pre-signed URLs
+//       const getUrl = `http://localhost:3001/users/getPresignedUrls?numFiles=${numFiles}`;
+//       const response = await fetch(getUrl);
+  
+//       if (response.ok) {
+//         const data = await response.json();
+//         if (data.urls) {
+//           setFileUrls(data.urls);
+  
+//           // Upload the selected files to the pre-signed URLs
+//           selectedFiles.forEach(async (file, index) => {
+//             const preSignedUrl = data.urls[index];
+//             await uploadFileToS3(file, preSignedUrl);
+//           });
+//         } else {
+//           console.error('No pre-signed URLs received from the server.');
+//         }
+//       } else {
+//         console.error('Error getting pre-signed URLs:', response.status, response.statusText);
+//       }
+//     } catch (error) {
+//       console.error('Error getting pre-signed URLs:', error);
+//     }
+//   };
+  
+
+//   const uploadFileToS3 = async (file, preSignedUrl) => {
+//     try {
+//       const response = await fetch(preSignedUrl, {
+//         method: 'PUT',
+//         body: file,
+//       });
+
+//       if (response.ok) {
+//         console.log(`File uploaded successfully to S3.`);
+//       } else {
+//         console.error('Failed to upload file to S3.');
+//       }
+//     } catch (error) {
+//       console.error('Error uploading file:', error);
+//     }
+//   };
+
+//   return (
+//     <div className="form-container">
+//       <form id="fileUploadForm" onSubmit={handleSubmit}>
+//         <div className="form-group">
+//           <label htmlFor="firstName" style={{ fontSize: "larger" }}>
+//             Name to be associated with job
+//           </label>
+//           <input
+//             type="text"
+//             id="firstName"
+//             name="firstName"
+//             placeholder="Your Name"
+//             value={name}
+//             onChange={handleInputChange}
+//             style={{ fontSize: "larger" }}
+//             required={true}
+//           />
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="selection" style={{ fontSize: "larger" }}>
+//             Select the number of files for compression
+//           </label>
+//           <select
+//             id="selection"
+//             name="selection"
+//             value={numFiles}
+//             onChange={handleFileChange}
+//             style={{ fontSize: "larger" }}
+//             required={true}
+//           >
+//             {/* <option value="0">0</option> */}
+//             <option value="1">1</option>
+//             <option value="2">2</option>
+//             <option value="3">3</option>
+//             <option value="4">4</option>
+//             <option value="5">5</option>
+//           </select>
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="filesForUpload" style={{ fontSize: "larger" }}>
+//             Select files for upload
+//           </label>
+//           <input
+//             type="file"
+//             name="filesForUpload"
+//             multiple
+//             required={true}
+//             style={{
+//               fontSize: "larger",
+//               alignContent: "center",
+//             }}
+//             onChange={handleFileSelect}
+//           />
+//         </div>
+//         <button type="submit" className="submit-button">
+//           ZipIt!
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Uploader;
+
 import React, { useState } from 'react';
-import axios from 'axios';
 
 const Uploader = () => {
   const [name, setName] = useState('');
   const [numFiles, setNumFiles] = useState(0);
-  const [fileUrls, setFileUrls] = useState([]); // To store pre-signed URLs
   const [selectedFiles, setSelectedFiles] = useState([]);
 
   const handleInputChange = (event) => {
@@ -132,48 +265,31 @@ const Uploader = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
-    try {
-      // Make a request to your server to get pre-signed URLs
-      const getUrl = `http://localhost:3001/users/getPresignedUrls?numFiles=${numFiles}`;
-      const response = await fetch(getUrl);
-  
-      if (response.ok) {
-        const data = await response.json();
-        if (data.urls) {
-          setFileUrls(data.urls);
-  
-          // Upload the selected files to the pre-signed URLs
-          selectedFiles.forEach(async (file, index) => {
-            const preSignedUrl = data.urls[index];
-            await uploadFileToS3(file, preSignedUrl);
-          });
-        } else {
-          console.error('No pre-signed URLs received from the server.');
-        }
-      } else {
-        console.error('Error getting pre-signed URLs:', response.status, response.statusText);
-      }
-    } catch (error) {
-      console.error('Error getting pre-signed URLs:', error);
-    }
-  };
-  
 
-  const uploadFileToS3 = async (file, preSignedUrl) => {
     try {
-      const response = await fetch(preSignedUrl, {
-        method: 'PUT',
-        body: file,
+      // Create a FormData object to send files to the backend
+      const formData = new FormData();
+      formData.append('name', name);
+      formData.append('numFiles', numFiles);
+
+      // Append selected files to the FormData object
+      for (const file of selectedFiles) {
+        formData.append('files', file);
+      }
+
+      // Make a POST request to your backend
+      const response = await fetch('http://localhost:3001/users/uploadToS3', {
+        method: 'POST',
+        body: formData,
       });
 
       if (response.ok) {
-        console.log(`File uploaded successfully to S3.`);
+        console.log('Files uploaded to S3 successfully.');
       } else {
-        console.error('Failed to upload file to S3.');
+        console.error('Error uploading files to S3:', response.status, response.statusText);
       }
     } catch (error) {
-      console.error('Error uploading file:', error);
+      console.error('Error uploading files to S3:', error);
     }
   };
 
@@ -207,7 +323,7 @@ const Uploader = () => {
             style={{ fontSize: "larger" }}
             required={true}
           >
-            {/* <option value="0">0</option> */}
+            <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -232,7 +348,7 @@ const Uploader = () => {
           />
         </div>
         <button type="submit" className="submit-button">
-          ZipIt!
+          Upload to S3
         </button>
       </form>
     </div>
@@ -240,4 +356,5 @@ const Uploader = () => {
 };
 
 export default Uploader;
+
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-
+const multer = require('multer');
 const AWS = require("aws-sdk");
 require("dotenv").config();
 
@@ -14,7 +14,9 @@ AWS.config.update({
 
 // create a unique bucket name
 const bucketName = "zipit-storage";
-const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+const s3 = new AWS.S3();
+const upload = multer({ dest: 'temp/' });
+
 
 (async () => {
   try {
@@ -37,40 +39,71 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
-// Endpoint to get pre-signed URLs for file uploads using a GET request
-router.get("/getPresignedUrls", (req, res) => {
-  const { numFiles } = req.query; // Number of files to upload
+// // Endpoint to get pre-signed URLs for file uploads using a GET request
+// router.get("/getPresignedUrls", (req, res) => {
+//   const { numFiles } = req.query; // Number of files to upload
 
-  if (!numFiles) {
-    return res
-      .status(400)
-      .json({ error: "Please specify the number of files to upload" });
+//   if (!numFiles) {
+//     return res
+//       .status(400)
+//       .json({ error: "Please specify the number of files to upload" });
+//   }
+
+//   const preSignedUrls = [];
+
+//   // Generate pre-signed URLs for each file
+//   for (let i = 0; i < numFiles; i++) {
+//     const params = {
+//       Bucket: "zipit-storage",
+//       Key: `uploads/${Date.now()}-file${i}`, // Set the key
+//       Expires: 600, // URL expiration time in seconds
+//     };
+
+//     s3.getSignedUrl("putObject", params, (error, url) => {
+//       if (error) {
+//         console.error("Error generating pre-signed URL:", error);
+//         return res
+//           .status(500)
+//           .json({ error: "Failed to generate pre-signed URL" });
+//       }
+//       preSignedUrls.push(url);
+
+//       // If all URLs have been generated, return them to the client
+//       if (preSignedUrls.length === numFiles) {
+//         res.json({ urls: preSignedUrls });
+//       }
+//     });
+//   }
+// });
+
+
+router.post('/uploadToS3', upload.array('files', 5), async (req, res) => {
+  const { name, numFiles } = req.body;
+  const uploadedFiles = req.files;
+
+  if (!name || !numFiles || !uploadedFiles) {
+    return res.status(400).json({ error: 'Invalid request data' });
   }
 
-  const preSignedUrls = [];
+  try {
+    const s3UploadPromises = [];
 
-  // Generate pre-signed URLs for each file
-  for (let i = 0; i < numFiles; i++) {
-    const params = {
-      Bucket: "zipit-storage",
-      Key: `uploads/${Date.now()}-file${i}`, // Set the key
-      Expires: 600, // URL expiration time in seconds
-    };
+    for (const file of uploadedFiles) {
+      const params = {
+        Bucket: 'zipit-storage',
+        Key: `uploads/${Date.now()}-${file.originalname}`,
+        Body: file.buffer, // Uploaded file data
+      };
 
-    s3.getSignedUrl("putObject", params, (error, url) => {
-      if (error) {
-        console.error("Error generating pre-signed URL:", error);
-        return res
-          .status(500)
-          .json({ error: "Failed to generate pre-signed URL" });
-      }
-      preSignedUrls.push(url);
+      s3UploadPromises.push(s3.upload(params).promise());
+    }
 
-      // If all URLs have been generated, return them to the client
-      if (preSignedUrls.length === numFiles) {
-        res.json({ urls: preSignedUrls });
-      }
-    });
+    await Promise.all(s3UploadPromises);
+
+    res.status(200).json({ message: 'Files uploaded to S3 successfully' });
+  } catch (error) {
+    console.error('Error uploading files to S3:', error);
+    res.status(500).json({ error: 'Failed to upload files to S3' });
   }
 });
 
